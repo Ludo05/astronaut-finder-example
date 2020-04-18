e329731fb470a66fc7037a73713d3bd4138a8726
Lewis Williams <lewis.williams@transformuk.com>
2020-04-18 14:35:48 +0100

    test fixes

--- src/test/java/com/astronautfinder/whosinspace/AstronautServiceTest.java
+++ src/test/java/com/astronautfinder/whosinspace/AstronautServiceTest.java
@@ -2,42 +2,45 @@ package com.astronautfinder.whosinspace;
 import com.astronautfinder.whosinspace.models.ClientAstronautDTO;
 import com.astronautfinder.whosinspace.services.AstronautServiceImpl;
 import com.astronautfinder.whosinspace.mocks.AstronautMockObj;
-import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class AstronautServiceTest {
 
-    @Mock private AstronautMockObj astronautMockObj;
 
     @Mock
     RestTemplate restTemplate;
 
-    @InjectMocks
-    private AstronautServiceImpl astronautService = new AstronautServiceImpl(restTemplate);
+    private AstronautServiceImpl astronautService;
+
+    @BeforeEach
+     void beforeAllTest() {
+          astronautService = new AstronautServiceImpl(restTemplate);
+    }
 
     @Test
     public void AddAstronautTest(){
         ClientAstronautDTO clientAstronautDTO = new ClientAstronautDTO("TestCraft","TestName");
         Mockito
                 .when(restTemplate.getForEntity("http://localhost:9090/astronaut/add", ClientAstronautDTO.class))
-          .thenReturn(new ResponseEntity(clientAstronautDTO, HttpStatus.OK));
-//        System.out.println(result);
-//        assertEquals("Success",result);
+                .thenReturn(new ResponseEntity(clientAstronautDTO, HttpStatus.OK));
+
+       String result = astronautService.getNumberOfAstronauts();
+       System.out.println(result);
+       assertEquals("Success",result);
     }
 
     @Test
